03873989b54c3c59f3004df9c057b18c49e9facc
tobinsouth <tobin.south@gmail.com>
2022-01-09 19:51:50 +0000

    Add Tensorboard & Validation

--- scripts/training.py
+++ scripts/training.py
@@ -7,7 +7,8 @@ from dataloader import get_train_test
 trainStays, testStays = get_train_test(train_size=0.7, batch_size=batch_size, shuffle=False, dataset='foresquare')
 
 # Load and define the model
-from VAE import SentenceVAE
+from VAE import SentenceVAE, device 
+
 # Model params
 params = dict(
     vocab_size = testStays.dataset._vocab_size,
@@ -26,17 +27,9 @@ params = dict(
     unk_idx=1,
 )
 model = SentenceVAE(**params)
+model = model.to(device) # Device is defined in VAE
 
-device = "cuda" # torch.device("cuda" if torch.cuda.is_available() else "cpu") # cuda is being a pain
-model = model.to(device)
-
-
-# Training
-
-epochs = 10
-learning_rate = 0.001
-k = 0.0025
-x0 =2500
+# Custom loss function from paper
 
 NLL = torch.nn.NLLLoss(ignore_index=0, reduction='sum')
 
@@ -54,11 +47,24 @@ def loss_fn(logp, target, mean, logv, step, k, x0):
 
     return NLL_loss, KL_loss, KL_weight
 
-optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
 
-step = 0
+
+# Training
+from torch.utils.tensorboard import SummaryWriter
+writer = SummaryWriter('runs/foresquare')
+
+learning_rate = 0.001
+optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+k = 0.0025
+x0 =2500
+epochs = 10
+
+# Run training loop
+step, running_loss = 0, 0 
 for epoch in range(epochs):
-    for iteration, batch in tqdm(enumerate(trainStays)):
+    for i, batch in enumerate(tqdm(trainStays)):
+
+        batch = batch.to(device)
         # Forward pass
         logp, mean, logv, z = model(batch)
 
@@ -73,3 +79,22 @@ for epoch in range(epochs):
         loss.backward()
         optimizer.step()
         step += 1
+
+        running_loss += loss.item()
+        if i % 1000 == 9999:  
+            writer.add_scalar('training loss',  running_loss / 1000, epoch * len(trainStays) + i)
+
+    for i, batch in enumerate(tqdm(testStays)):
+        batch = batch.to(device)
+        logp, mean, logv, z = model(batch)
+
+        # loss calculation
+        NLL_loss, KL_loss, KL_weight = loss_fn(logp, batch, mean, logv, step, k, x0)
+        loss = (NLL_loss + KL_weight * KL_loss) / batch_size
+
+        running_loss += loss.item()
+    writer.add_scalar('validation loss',  running_loss / len(testStays), epoch * len(testStays))
+
+writer.close()
+
+        
