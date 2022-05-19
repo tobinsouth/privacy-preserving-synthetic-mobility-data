55d0d042b9331201ba78959fb2f0bdb9f2463f5e
tobinsouth <tobin.south@gmail.com>
2022-05-19 18:41:10 +0000

    Adding MoveSim work

---
+++ MoveSim/code/gen_data.py
@@ -0,0 +1,52 @@
+from evaluations import *
+from utils import *
+from tqdm import tqdm
+
+def distance(gps1,gps2):
+    x1,y1 = gps1
+    x2,y2 = gps2
+    return np.sqrt((x1-x2)**2+(y1-y2)**2 )
+
+    
+    
+
+def gen_matrix(data='geolife'):
+    train_data = read_data_from_file('../data/%s/real.data'%data)
+    gps = get_gps('../data/%s/gps'%data)
+    max_locs = len(gps[0])
+
+    print('Generating Matrices...')
+    # Essentially, the transition matrix
+    reg1 = np.zeros([max_locs,max_locs]) 
+    for i in range(len(train_data)):
+        line = train_data[i]
+        for j in range(len(line)-1):
+            reg1[line[j],line[j+1]] +=1
+
+    print('Generated Transition Matrix')
+    # Now we create a distance matrix
+    # reg2 = np.zeros([max_locs,max_locs])
+    # for i in tqdm(range(max_locs)):
+    #     for j in range(max_locs):
+    #         if i!=j:
+    #             reg2[i,j] = distance((gps[0][i],gps[1][i]),(gps[0][j],gps[1][j]))
+    from scipy.spatial.distance import cdist 
+    gps_np = np.vstack([gps[0], gps[1]]).T
+    reg2 = cdist(gps_np,gps_np)
+    print(gps_np.shape)
+    print('Generated distance matrix')
+
+    
+    np.save('../data/%s/M1.npy'%data,reg1)
+    np.save('../data/%s/M2.npy'%data,reg2)
+
+    print('Matrix Generation Finished')
+
+    
+
+    
+
+
+
+
+    
