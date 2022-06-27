78e06a19e8956d86dc56c07994b7346f59bd16a1
Tobin South <tobin.south@gmail.com>
2022-06-27 11:20:05 -0400

    Fixing SettingWithCopyWarning

--- scripts/data_chunking.py
+++ scripts/data_chunking.py
@@ -88,7 +88,7 @@ def get_all_sequences(user_df, ):
     Expected to be applied to a pandas.DataFrameGroupBy object.
     """
     user_sequences = []
-    start_date = (user_df['datetime'].iloc[0].normalize() + pd.Timedelta(hours=4)).timestamp() # Get 4am of the first day
+    start_date = (user_df['dt'].iloc[0].normalize() + pd.Timedelta(hours=4)).timestamp() # Get 4am of the first day
 
 
     while len(user_df) > 0: # We progressively filter off days as we go along
@@ -110,11 +110,11 @@ def get_all_sequences(user_df, ):
             break
         
         # Get next start date (earliest date more that 24 hours away)
-        start_date = (user_df['datetime'].iloc[0].normalize() + pd.Timedelta(hours=4)).timestamp()
+        start_date = (user_df['dt'].iloc[0].normalize() + pd.Timedelta(hours=4)).timestamp()
 
     return user_sequences
 
-
+print('Looking for data in `{}`'.format(data_directory))
 print("Reading in large stays data file...")
 all_stays_csvs = glob.glob(data_directory+'/*stays*1.csv.gz') # This will look for all of the stays1.csv.gz files in the data directory. Stay2 is old data.
 stays = pd.read_csv(all_stays_csvs[0]) # Currently we only have a single file, but this is ready to be expanded to multiple files.
@@ -139,12 +139,12 @@ else:
 print("Total original data points: %d \nFiltered total data points: %d \nPercentage reduced %.4f \nNumber of unique users: %d" % 
 (len(stays), len(filtered_stays), 1-len(filtered_stays)/len(stays), len(filtered_stays['user'].unique())))
 
-filtered_stays['datetime'] = pd.to_datetime(filtered_stays['ini_dat'], unit='s', utc=True) + pd.Timedelta(hours=-7) # Set timezone to PST
-filtered_stays['finishtime'] = filtered_stays['datetime'] + filtered_stays['duration'].apply(pd.Timedelta, unit='s')
-filtered_stays['ini_dat'] = filtered_stays['datetime'].apply(lambda dt: dt.timestamp())
+filtered_stays['dt'] = pd.to_datetime(filtered_stays['ini_dat'], unit='s', utc=True) + pd.Timedelta(hours=-7) # Set timezone to PST
+filtered_stays['finishtime'] = filtered_stays['dt'] + filtered_stays['duration'].apply(pd.Timedelta, unit='s')
+filtered_stays['ini_dat'] = filtered_stays['dt'].apply(lambda dt: dt.timestamp())
 filtered_stays['fin_dat'] = filtered_stays['finishtime'].apply(lambda dt: dt.timestamp())
-filtered_stays.sort_values(by='datetime', inplace=True)
-filtered_stays = filtered_stays[['datetime','ini_dat', 'fin_dat', 'user', 'GEOID']]
+filtered_stays.sort_values(by='dt', inplace=True)
+filtered_stays = filtered_stays[['dt','ini_dat', 'fin_dat', 'user', 'GEOID']]
 filtered_grouped_users = filtered_stays.groupby('user')
 
 
